06702ec0ea082ed2a0e715b021a4a7738847b808
Mufil Rahman A <ramanizum@gmail.com>
2024-01-06 20:33:40 +0530

    Mui Paper Component

--- src/components/MuiLayout.jsx
+++ src/components/MuiLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Box, Stack, Divider, Grid } from "@mui/material";
+import { Box, Stack, Divider, Grid, Paper } from "@mui/material";
 
 const MuiLayout = () => {
   return (
-    <>
-      {" "}
+    <Paper
+      sx={{
+        p: "32px",
+      }}
+      elevation={4}
+    >
       <Stack
         sx={{
           border: "1px solid",
@@ -34,28 +38,28 @@ const MuiLayout = () => {
         ></Box>
       </Stack>
       <Grid container my={4} rowSpacing={2} columnSpacing={4}>
-        <Grid item xs={3} sm = {6} lg={12}>
+        <Grid item xs={3} sm={6} lg={12}>
           <Box bgcolor="primary.light" p={2}>
             Item 1
           </Box>
         </Grid>
-        <Grid item xs={9} sm = {6} lg={12}>
+        <Grid item xs={9} sm={6} lg={12}>
           <Box bgcolor="secondary.light" p={2}>
             Item 2
           </Box>
         </Grid>
-        <Grid item xs={9} sm = {6} lg={12}>
+        <Grid item xs={9} sm={6} lg={12}>
           <Box bgcolor="warning.light" p={2}>
             Item 3
           </Box>
         </Grid>
-        <Grid item xs={3} sm = {6} lg={12}>
+        <Grid item xs={3} sm={6} lg={12}>
           <Box bgcolor="error.light" p={2}>
             Item 4
           </Box>
         </Grid>
       </Grid>
-    </>
+    </Paper>
   );
 };
 
